c1efb9104b4b2bcd4a0f1c226ca70e234efb6fc5
Kevin Wu Won <kevin@wuwon.id.au>
2010-12-05 23:28:28 +1000

    Enable concurrency.
    Generate the picture in another thread and post updates to the GUI

--- mandelbrot.hs
+++ mandelbrot.hs
@@ -5,12 +5,13 @@ import Control.Concurrent.MVar
 import Data.Array.MArray
 import Data.Word
 import Control.Monad
+import Control.Concurrent
 
 red = Color 65535 0 0
 white = Color 65535 65535 65535
 
-width = 525 :: Int
-height = 300 :: Int
+windowWidth = 525 :: Int
+windowHeight = 300 :: Int
 
 main :: IO ()
 main = do
@@ -19,29 +20,34 @@ main = do
     windowSetTitle win "The Mandelbrot Set"
     win `onDestroy` mainQuit
 
-    mandelbrot <- makeMandelbrotPixbuf
+    mandelPb <- newMVar =<< pixbufNew ColorspaceRgb False 8 windowWidth windowHeight
  
     canvas <- drawingAreaNew
-    canvas `onSizeRequest` return (Requisition width height)
-    canvas `onExpose` drawCanvas canvas mandelbrot
+    canvas `onSizeRequest` return (Requisition windowWidth windowHeight)
+    canvas `onExpose` drawCanvasHandler canvas mandelPb
+
+    forkIO $ makeMandelbrotPixbuf canvas mandelPb
  
     containerAdd win canvas
     widgetShowAll win
     mainGUI
 
-makeMandelbrotPixbuf :: IO Pixbuf
-makeMandelbrotPixbuf = do
-    pb <- pixbufNew ColorspaceRgb False 8 width height
+makeMandelbrotPixbuf :: DrawingArea -> MVar Pixbuf -> IO ()
+makeMandelbrotPixbuf canvas pbVar = do
+    pb <- readMVar pbVar
     pbData <- (pixbufGetPixels pb :: IO (PixbufData Int Word8))
     rowStride <- pixbufGetRowstride pb
-    forM_ [0..(width-1)] (\c ->
-        forM_ [0..(height-1)] (\r ->
+    putStrLn "calculating..."
+    forM_ [0..(windowWidth-1)] (\c -> do
+        forM_ [0..(windowHeight-1)] (\r ->
             let v = mandelbrot (c, r) in do
                 writeArray pbData (c*3 + r*rowStride) v
                 writeArray pbData (c*3 + r*rowStride+1) v
                 writeArray pbData (c*3 + r*rowStride+2) v
-            ))
-    return pb
+            )
+        postGUIAsync $ do {drawCanvas canvas pbVar c 0 1 windowHeight; return ()})
+    putStrLn "done"
+    return ()
 
 mandelbrot :: (Int, Int) -> Word8
 mandelbrot (x, y) =
@@ -53,18 +59,21 @@ mandelbrot (x, y) =
               else fromIntegral $ max 0 $ 256 - 8*(length left)
 
 scaleX :: Int -> Double
-scaleX x = (fromIntegral x)/(fromIntegral width)*3.5 - 2.5
+scaleX x = (fromIntegral x)/(fromIntegral windowWidth)*3.5 - 2.5
 
 scaleY :: Int -> Double
-scaleY y = -((fromIntegral y)/(fromIntegral height)*2 - 1)
+scaleY y = -((fromIntegral y)/(fromIntegral windowHeight)*2 - 1)
 
 p :: (Double, Double) -> (Double, Double) -> (Double, Double)
 p (x0, y0) (x, y) = (x*x - y*y + x0, 2*x*y + y0)
 
-drawCanvas :: DrawingArea -> Pixbuf -> event -> IO Bool
-drawCanvas canvas pb _evt = do
+drawCanvasHandler :: DrawingArea -> MVar Pixbuf -> event -> IO Bool
+drawCanvasHandler canvas pbVar _evt = drawCanvas canvas pbVar 0 0 windowWidth windowHeight
+
+drawCanvas :: DrawingArea -> MVar Pixbuf -> Int -> Int -> Int -> Int -> IO Bool
+drawCanvas canvas pbVar x y width height = do 
+    pb <- readMVar pbVar
     dw <- widgetGetDrawWindow canvas
-    drawWindowClear dw
     gc <- gcNew dw
-    drawPixbuf dw gc pb 0 0 0 0 width height RgbDitherNone 0 0
+    drawPixbuf dw gc pb x y x y width height RgbDitherNone 0 0
     return True -- everything is OK
